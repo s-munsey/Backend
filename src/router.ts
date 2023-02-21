97ca9e2606ae27508389032e49a0e51cdf30e62c
Shaun Munsey <s_munsey@live.com>
2023-02-21 23:14:55 +0000

    basic validation

--- src/router.ts
+++ src/router.ts
@@ -1,20 +1,35 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = Router();
 
 /**
  * Book // may move get all outside as it doesn't need auth
  */
-router.get("/books", (req, res) => {
+router.get("/books", (req: Request, res: Response) => {
   res.json({ message: "book" });
 });
 
-router.get("/books/:id", (req, res) => {});
+router.get("/books/:id", (req: Request, res: Response) => {});
 
-router.post("/books", (req, res) => {});
+router.post("/books", body("title"), (req: Request, res: Response) => {
+  const errors = validationResult(req);
 
-router.put("/books/:id", (req, res) => {});
+  if (!errors.isEmpty()) {
+    res.status(400);
+    res.json({ errors: errors.array() });
+  }
+});
 
-router.delete("/books/:id", (req, res) => {});
+router.put("/books/:id", body("title"), (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(400);
+    res.json({ errors: errors.array() });
+  }
+});
+
+router.delete("/books/:id", (req: Request, res: Response) => {});
 
 export default router;

07ce71d74b63e03ab164c27a3040b387db6cb6b3
Shaun Munsey <s_munsey@live.com>
2023-02-21 23:26:58 +0000

    input error middleware

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import { body, validationResult } from "express-validator";
+import { handleInputErrors } from "./modules/middleware";
 
 const router = Router();
 
@@ -15,27 +16,17 @@ router.get("/books/:id", (req: Request, res: Response) => {});
 router.post(
   "/books",
   body("title").isAlphanumeric(),
-  (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      res.status(400);
-      res.json({ errors: errors.array() });
-    }
-  }
+  body("author").isString(),
+  handleInputErrors,
+  (req: Request, res: Response) => {}
 );
 
 router.put(
   "/books/:id",
   body("title").isAlphanumeric(),
-  (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      res.status(400);
-      res.json({ errors: errors.array() });
-    }
-  }
+  body("author").isString(),
+  handleInputErrors,
+  (req: Request, res: Response) => {}
 );
 
 router.delete("/books/:id", (req: Request, res: Response) => {});
